a88c7bc927f8099a38fca9012e92a9b98523227d
Aditya <mamtasinghahana123@gmail.com>
2025-05-24 16:32:15 +0530

    Project frontend setup done

---
+++ frontend/src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+
+export const App = () => {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </BrowserRouter>
+  );
+};
+
+export default App;
