e5f70ba7dea02e6420af4340881cf923b7fffc5e
prashant9960 <kalapahadprashant@gmail.com>
2025-05-25 10:16:23 +0530

    cartdrawer

--- frontend/src/components/Layout/CartDrawer.jsx
+++ frontend/src/components/Layout/CartDrawer.jsx
@@ -1,10 +1,16 @@
 import React from 'react'
 import {IoMdClose} from "react-icons/io"
 import CartContents from '../Cart/CartContents'
+import { useNavigate } from 'react-router-dom';
+
 
 const CartDrawer = ({drawerOpen, toggleCartDrawer }) => {
 
-    
+  const navigate = useNavigate();
+  const handleCheckout = () => {
+    toggleCartDrawer()
+    navigate("/checkout")
+  }
   return (
     <div className={`fixed top-0 right-0 w-2/4 sm:w-1/2 md:w-[30rem] h-full bg-white shadow-lg transform transition-transform duration-300 flex flex-xol z-50 ${
         drawerOpen ? "translate-x-0" : "translate-x-full"
@@ -19,7 +25,13 @@ const CartDrawer = ({drawerOpen, toggleCartDrawer }) => {
             <h2 className='text-xl font-semibold mb-4'>Your Saved</h2>
             <CartContents />
         </div>
+        
         <div className="fixed bottom-0 left-0 w-full bg-white p-4 z-50 shadow border-t border-gray-200">
+        <div className ="p-4 bg-white sticky bottom-0">
+          <button onClick={handleCheckout} className='w-full bg-black text-white py-3 rounded-lg font-semibold hover:bg-gray-800'>
+            Start Reporting
+          </button>  
+        </div> 
   <p className="text-sm tracking-tighter text-gray-700 text-center">
     Report It Before Someone Else Gets Paid.
   </p>
